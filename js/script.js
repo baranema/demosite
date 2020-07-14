ef2ad5def626a54491349bc9c9f26a6b9bd6ab65
Ema Baranauskaite <emabaranauskaite@MacBook-Pro-Ema.lan>
2020-07-14 14:59:50 +0300

    third

--- js/script.js
+++ js/script.js
@@ -8,4 +8,10 @@ $(document).ready(function() {
         var leftHeight = $('.header-content-2').height();
         $('.header-content-2').css({'height':leftHeight});
     });
+
+    $.get('https://jsonplaceholder.typicode.com/posts', function(data) {
+        for (var i = 0; i < 4; i++) {
+            $('#list').append("<li>" + data[i].title +  "</li"); 
+        } 
+    }); 
 });
