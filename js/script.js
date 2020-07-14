05c6624108311844b2f34814bafcb9ea2afcb6b8
Ema Baranauskaite <emabaranauskaite@MacBook-Pro-Ema.lan>
2020-07-14 13:12:38 +0300

    sec

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {  
+    $(document).ready(function() {
+        var leftHeight = $('.header-content-2').height();
+        $('.header-content-1').css({'height':leftHeight});
+    });
+
+    $(document).ready(function() {
+        var leftHeight = $('.header-content-2').height();
+        $('.header-content-2').css({'height':leftHeight});
+    });
+});
